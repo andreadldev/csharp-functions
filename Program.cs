60e40c70a28bc36274febad8f463483e1fb18b11
andreadldev <andrea.delisi2@gmail.com>
2023-03-17 14:24:17 +0100

    Funzione per stampare array

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+int[] numbers = { 2, 6, 7, 5, 3, 9 };
+StampaArray(numbers);
+
+void StampaArray(int[] array)
+{
+    Console.Write("[");
+	for (int i = 0; i < array.Length; i++)
+	{
+		Console.Write($"{array[i]}");
+		if (i != array.Length - 1)
+		{
+			Console.Write(", ");
+		}
+	}
+    Console.Write("]");
+}
